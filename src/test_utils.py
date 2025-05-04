d71956130a78df5ef234460d704202c7eb0d323b
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-05-04 12:37:39 -0400

    Add split_nodes_delimiter function and corresponding tests for text node delimiters

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNodeToHtmlNode(unittest.TestCase):
@@ -52,3 +52,140 @@ class TestTextNodeToHtmlNode(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             text_node_to_html_node(node)
         self.assertTrue("Unknown text type" in str(context.exception))
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_asterisk(self):
+        # Test with asterisk delimiter for bold text
+        nodes = [TextNode("This is *bold* text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " text")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_underscore(self):
+        # Test with underscore delimiter for italic text
+        nodes = [TextNode("This is _italic_ text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "italic")
+        self.assertEqual(result[1].text_type, TextType.ITALIC)
+        self.assertEqual(result[2].text, " text")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_backtick(self):
+        # Test with backtick delimiter for code text
+        nodes = [TextNode("This is `code` text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "code")
+        self.assertEqual(result[1].text_type, TextType.CODE)
+        self.assertEqual(result[2].text, " text")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_multiple_delimiters(self):
+        # Test text with multiple delimiter pairs
+        nodes = [TextNode("*Bold* text and more *bold* text", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, "")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "Bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " text and more ")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+        self.assertEqual(result[3].text, "bold")
+        self.assertEqual(result[3].text_type, TextType.BOLD)
+        self.assertEqual(result[4].text, " text")
+        self.assertEqual(result[4].text_type, TextType.TEXT)
+
+    def test_no_delimiters(self):
+        # Test text with no delimiters
+        nodes = [TextNode("Plain text without delimiters", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].text, "Plain text without delimiters")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+
+    def test_adjacent_delimiters(self):
+        # Test text with adjacent delimiter pairs
+        nodes = [TextNode("This text has *bold**adjacent* markers", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].text, "This text has ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, "")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+        self.assertEqual(result[3].text, "adjacent")
+        self.assertEqual(result[3].text_type, TextType.BOLD)
+        self.assertEqual(result[4].text, " markers")
+        self.assertEqual(result[4].text_type, TextType.TEXT)
+
+    def test_empty_delimited_text(self):
+        # Test empty text between delimiters
+        nodes = [TextNode("This has an ** empty bold section", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This has an ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " empty bold section")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_mixed_node_types(self):
+        # Test with a mix of text nodes and other types
+        text_node = TextNode("This is *bold* text", TextType.TEXT)
+        bold_node = TextNode("Already bold", TextType.BOLD)
+        nodes = [text_node, bold_node]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " text")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+        self.assertEqual(result[3].text, "Already bold")
+        self.assertEqual(result[3].text_type, TextType.BOLD)
+
+    def test_uneven_delimiters(self):
+        # Test exception with uneven number of delimiters
+        nodes = [TextNode("This text has *uneven delimiters", TextType.TEXT)]
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertTrue("uneven number of *" in str(context.exception))
+
+    def test_special_character_delimiter(self):
+        # Test with a special character as delimiter
+        nodes = [TextNode("This has a $special$ delimiter", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "$", TextType.CODE)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This has a ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "special")
+        self.assertEqual(result[1].text_type, TextType.CODE)
+        self.assertEqual(result[2].text, " delimiter")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_delimiter_at_start_and_end(self):
+        # Test delimiters at the start and end of text
+        nodes = [TextNode("*Bold text at the start and end*", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "Bold text at the start and end")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, "")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from leafnode import LeafNode
-from textnode import TextType
+from textnode import TextNode, TextType
 
 
 def text_node_to_html_node(text_node):
@@ -21,4 +21,29 @@ def text_node_to_html_node(text_node):
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    pass
+    new_nodes = []
+    for node in old_nodes:
+        if (
+            isinstance(node, TextNode)
+            and node.text_type == TextType.TEXT
+            and delimiter in node.text
+        ):
+            delimiter_count = 0
+            for char in node.text:
+                if char == delimiter:
+                    delimiter_count += 1
+            if delimiter_count % 2 != 0:
+                raise Exception(
+                    f"You have an uneven number of {delimiter} in your text: {node.text}"
+                )
+
+            parts = node.text.split(delimiter)
+            for i in range(len(parts)):
+                if i % 2 != 0:
+                    new_nodes.append(TextNode(parts[i], text_type))
+                else:
+                    new_nodes.append(TextNode(parts[i], TextType.TEXT))
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
