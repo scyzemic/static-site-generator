15890b39cd393456d6d615f5c11dca36e15db10d
scyzemic <11906352+scyzemic@users.noreply.github.com>
2025-05-04 11:47:03 -0400

    moving utils to their own files

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from main import text_node_to_html_node
+from utils import text_node_to_html_node
 
 
 class TestTextNodeToHtmlNode(unittest.TestCase):
